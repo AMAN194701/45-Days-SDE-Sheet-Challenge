6b8d4be5071c7657a5c3d844fd183e740050c8f8
Aman Kushwaha <amankushwaha61709@gmail.com>
2026-07-16 19:09:05 +0530

    BTS iterator

---
+++ Day_44/2_BST_iterator.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class BSTIterator:
+
+    def __init__(self, root):
+        self.stack =[]
+        self.pushAll(root)
+    def pushAll(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+        
+    def next(self) -> int:
+        node = self.stack.pop()
+        if node.right:
+            self.pushAll(node.right)
+        return node.val
+        
+    def hasNext(self) -> bool:
+        return len(self.stack) > 0
+        
